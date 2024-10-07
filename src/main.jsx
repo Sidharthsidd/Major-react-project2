fe4a6f343acbcf4cdb7ccb7c4dd7f1b154464e2d
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-10-08 01:02:20 +0530

    commitonemore

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './store/store.js'
+import Store from './Store/Store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
@@ -79,7 +79,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
     <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
