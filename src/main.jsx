f44df6bf298f38d8acedbe5a3e35dca4b9e419a6
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-10-10 18:15:22 +0530

    projet almost complete 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import Store from './Store/Store.js'
+import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
@@ -11,7 +11,7 @@ import { AuthLayout, Login } from './components/index.js'
 
 import AddPost from "./pages/AddPost";
 import Signup from './pages/Signup'
-import EditPost from "./pages/EditPost.jsx";
+import EditPost from "./pages/EditPost";
 
 import Post from "./pages/Post";
 
@@ -79,7 +79,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={Store}>
+    <Provider store={store}>
     <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
